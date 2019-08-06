93d2c228f34324d193adac7e590af0bf9ecbb5c3
Jess Sullivan <jess@sulliwood.org>
2019-08-06 17:54:17 +0000

    init docker

---
+++ Docker-App/pkgs.r
@@ -0,0 +1,3 @@
+install.packages(c('shiny', 'tidyverse', 'plyr', 'rgdal', 'gdalUtils'))
+install.packages(c('sf', 'geosphere', 'r2stl', 'png', 'imager'))
+install.packages(c('magick', 'shinycssloaders', 'rgl'))
