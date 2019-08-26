43e10466770920f823b1a1a0bb819be03a88ad4a
Jess <Jess@sulliwood.org>
2019-08-26 07:57:40 -0400

    + timeout musing

--- Flask_Manager/app.py
+++ Flask_Manager/app.py
@@ -6,20 +6,21 @@ WIP by Jess Sullivan
 from flask import Flask, send_file, redirect
 import subprocess
 import time, os
+import threading
 
 app = Flask(__name__)
 
 # setup workspace #
 
 base_url = 'http://127.0.0.1'
-work_path = '/Users/jesssullivan/git/Shiny-Apps/Flask'
-os.chdir(work_path)
+# work_path = ''
+# os.chdir(work_path)
 
 #  Manage ports #
 
 min_port = 5001
 max_port = 5011
-max_timeout = 600
+max_timeout = 5 # in secs
 all_ports = []
 
 # Run once:
@@ -28,19 +29,17 @@ for X in range(min_port, max_port):
 
 Active_Ports = {}
 
-"""
 # testing only #
 def fillports(numfill):
     iport = min_port
     for E in range(numfill):
         iport += 1
         Active_Ports[iport] = time.time() + E
-"""
 
 
 ## Manage commands, behavior ##
 
-def bash_command(cmd):
+def bash_run(cmd):
     subprocess.Popen(cmd, shell=True, executable='/bin/bash')
 
 
@@ -55,51 +54,53 @@ def port_generate():
         return curr_avail[1]
 
 
-def start_R(portnum, appchoice):
-    bash_command(str('Rscript shiny_apps/' + appchoice + '.R ' + str(portnum)))
+def start_R(appchoice):
+    portnum = port_generate()
+    bash_run(str('Rscript shiny_apps/' + appchoice + '.R ' + str(portnum)))
+    Active_Ports[portnum] = time.time()
+    time.sleep(2)
+    return redirect(str(base_url + ':' + str(portnum)))
 
+# TODO: Kill ports after max_timout
+"""
+tmp_time = time.time()
+def check_ports(tmp_time):
 
+    # check live ports:
+    tmp_keys = list(Active_Ports.keys())
+    for D in tmp_keys:
+        if Active_Ports[D] >= tmp_time + max_timeout:
+            bash_run(str('kill ' + str(D)))
+            tmp_time = time.time()
+            print(str('killed app on port ' + str(D) +
+                    'max time exceeded'))
+
+    threading.Timer(3, check_ports).start()
+
+check_ports(tmp_time)
+"""
 # Define routes + apps
 @app.route('/')
 def home():
     return send_file('home.html')
 
-
 @app.route('/default')
 def default():
-    tmp_port = port_generate()
-    start_R(tmp_port, 'default')
-    time.sleep(1)
-    return redirect(str(base_url + ':' + str(tmp_port)))
-
+    return start_R('default')
 
 @app.route('/DEM2STL')
 def DEM2STL():
-    tmp_port = port_generate()
-    start_R(tmp_port, 'DEM2STL')
-    time.sleep(2)
-    return redirect(str(base_url + ':' + str(tmp_port)))
-
+    return start_R('DEM2STL')
 
 @app.route('/KML2CSV')
 def KML2CSV():
-    tmp_port = port_generate()
-    start_R(tmp_port, 'KML2CSV')
-    time.sleep(2)
-    return redirect(str(base_url + ':' + str(tmp_port)))
-
+    return start_R('KML2CSV')
 
 @app.route('/CENTKML')
 def CENTKML():
-    tmp_port = port_generate()
-    start_R(tmp_port, 'CENTKML')
-    time.sleep(2)
-    return redirect(str(base_url + ':' + str(tmp_port)))
-
+    return start_R('CENTKML')
 
 @app.route('/KMLSUBSET')
 def KMLSUBSET():
-    tmp_port = port_generate()
-    start_R(tmp_port, 'KMLSUBSET')
-    time.sleep(2)
-    return redirect(str(base_url + ':' + str(tmp_port)))
+    return start_R('KMLSUBSET')
+
